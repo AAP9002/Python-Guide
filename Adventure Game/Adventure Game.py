eb33dd7301a83ec89474afd92b0907671b5d3d25
z72819ap <alan.prophett@student.manchester.ac.uk>
2022-11-02 23:06:29 +0000

    file handling and adventure game

--- Adventure Game/Adventure Game.py
+++ Adventure Game/Adventure Game.py
@@ -1,19 +1,25 @@
-### Adventure game - Alan Prophett ###
+#--- Adventure game - Alan Prophett ---
 import time,os
+
+def overwriteSettings(currentSettings):
+        with open("settings.txt",'w') as file:
+                for item in currentSettings:
+                        file.write("%s\n"%item)
 
 #loading screen
 print("Loading . . . . . . .")
-time.sleep(2)
+time.sleep(1)
 
-playerSettings = {
-	"name" : os.getlogin(),
-	"characterName" : "",
-	"characterAge" : 520
+playerSettings = list()
 
-}
+# with open("settings.txt") as file:
+#        playerSettings = file.readlines()
+
+playerSettings = [line.rstrip('\n') for line in open("settings.txt")]
 
 #intro and name
-print("Welcome to my Adventure Game", os.getlogin(),"!",'\n',"Choose your character:")
+# os.getlogin()
+print("Welcome to my Adventure Game",playerSettings[0] ,"!",'\n',"Choose your character:")
 
 #choose you character
 characterList = ["Knight","Warrior","Wizard"]#list of character types
@@ -26,8 +32,9 @@ characterList = ["Knight","Warrior","Wizard"]#list of character types
 for idx, val in enumerate(characterList,start=1):
 	print(str(idx)+ ". "+ val)
 
-playerSettings["characterName"] = characterList[int(input("Type in your character number:"))-1]
-print("you chose: " +playerSettings.get("characterName"))
+playerSettings[1] = characterList[int(input("Type in your character number:"))-1]
+print("you chose: " +playerSettings[1])
+overwriteSettings(playerSettings)
 
 
 settingsMenu = ["Player name","Character name","Character age", "View Current Settings"]
@@ -36,27 +43,30 @@ while True:
 	menu = int(input("Would you like to (1) start or (2) change settings?"))
 
 	if menu ==1:
-		move = input("\n "+ playerSettings.get("characterName")+", do you want to move?")
+		move = input("\n "+ playerSettings[1]+", do you want to move?")
 		count =0
 		while move in ('Yes','yes','Y','y'):
 			print("You've moved 1 space")
-			move = input(playerSettings.get("characterName")+", keep moving?")
+			count+=1
+			move = input(playerSettings[1]+", keep moving?")
 		else:
-			print("You moved "+count+" spaces")
-			break
+			print("You moved "+str(count)+" spaces")
 	elif menu ==2:
 		for idx, val in enumerate(settingsMenu,start=1):
 			print(str(idx)+ ". "+ val)
 		settingChange = int(input("What would you like the change?"))
 
 		if settingChange == 1:
-			playerSettings["name"] = input()
+			playerSettings[0] = input()
+			overwriteSettings(playerSettings)
 			print("changed")
 		elif settingChange == 2:
-			playerSettings["characterName"] = input()
+			playerSettings[1] = input()
+			overwriteSettings(playerSettings)
 			print("changed")
 		elif settingChange == 3:
-			playerSettings["characterAge"] = input()
+			playerSettings[2] = input()
+			overwriteSettings(playerSettings)
 			print("changed")
 		elif settingChange == 4:
 			print(playerSettings)
